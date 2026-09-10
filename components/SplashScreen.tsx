d1100024c9634e066248977cb318bbbb16da37a0
srGabrielx <gabriel094x@gmail.com>
2026-09-09 21:56:50 -0300

    style: change splash animation from green to purple

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -48,6 +48,7 @@ export default function SplashScreen({ onFinish }: { onFinish: () => void }) {
           height: "auto",
           maxWidth: "80%",
           objectFit: "contain",
+          filter: "hue-rotate(150deg)",
         }}
       >
         <source src="/off-the-wall.webm" type="video/webm" />
